18d37945366dd844aaa157bd599905a12e20a0bf
Hikaru Tomizawa <tomizawa.licht@gmail.com>
2025-02-11 23:42:16 +0900

    chore: initialize Spring Boot project with required dependencies

---
+++ src/main/java/com/automatedtomato/email_templates_management_app/EmailTemplatesManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.automatedtomato.email_templates_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailTemplatesManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailTemplatesManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automatedtomato/email_templates_management_app/EmailTemplatesManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automatedtomato.email_templates_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailTemplatesManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
